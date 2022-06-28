018fb4e3b587f80c87a8dfe17c591a4544670ef1
Carlo1891 <carlo.18@live.it>
2022-06-28 16:00:56 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Viene fornita una piccola tabella che rappresenta i membri di un team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// Completate i milestone nell'ordine assegnato, non andate avanti finché non avete concluso con successo la milestone precedente.
+// Chiudete almeno una commit per milestone.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+// Consigli del giorno:
+// Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+
